f68b400c5c7ff1e1f9295eae72bf7dabd1a9c27d
Nonu Unon <41541598+Nonunon@users.noreply.github.com>
2026-08-25 12:34:49 -0500

    Update SteamRelink.user.js
    PRETTY ICON

--- SteamRelink.user.js
+++ SteamRelink.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         SteamRelink Button
 // @namespace    https://steamre.link
-// @version      2.2
+// @version      2.3
 // @description  Adds a button to redirect Steam Workshop links to the custom SteamRelink page, and auto-closes SteamRelink fast-mode tabs shortly after steam:// fires (toggleable via the script manager's menu)
+// @icon         https://steamre.link/images/SteamRedirect-32x32.png
 // @updateURL    https://raw.githubusercontent.com/Nonunon/SteamRelink/refs/heads/main/SteamRelink.user.js
 // @downloadURL  https://raw.githubusercontent.com/Nonunon/SteamRelink/refs/heads/main/SteamRelink.user.js
 // @match        *://steamcommunity.com/sharedfiles/filedetails/?id=*
